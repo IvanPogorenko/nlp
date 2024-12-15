44c7065891cbe5f6c8d8e8137d16019da611e75e
IvanPogorenko <112338510+IvanPogorenko@users.noreply.github.com>
2024-12-15 19:14:27 +0700

    Update lab2.py

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,6 +1,6 @@
 import gensim
 
-pos = ["уазик_NOUN", "такси_NOUN"]
+pos = ["рафик_NOUN", "шофер_NOUN"]
 neg = []
 
 file = 'C:/Users/я/Desktop/учеба/обработка языка/cbow.txt'
@@ -11,16 +11,6 @@ with open(f'C:/Users/я/Desktop/учеба/обработка языка/nlp/lab
 word2vec = gensim.models.KeyedVectors.load_word2vec_format(file, binary=False)
 dist = word2vec.most_similar(positive=pos, negative = neg)
 with open(f'C:/Users/я/Desktop/учеба/обработка языка/nlp/lab2/output.txt', 'w', encoding='utf-8') as out:
-    out.write('Сложение\n')
-    for i in dist:
-        out.write(f'{i[0]}  {str(i[1])}\n')
-
-
-pos = ["уазик_NOUN"]
-neg = ["такси_NOUN"]
-word2vec = gensim.models.KeyedVectors.load_word2vec_format(file, binary=False)
-dist = word2vec.most_similar(positive=pos, negative = neg)
-with open(f'C:/Users/я/Desktop/учеба/обработка языка/nlp/lab2/output.txt', 'a', encoding='utf-8') as out:
-    out.write('Вычитание\n')
+    out.write('При сложении рафик_NOUN и шофер_NOUN получили нужные слова (уазик и такси) в первой десятке\n')
     for i in dist:
         out.write(f'{i[0]}  {str(i[1])}\n')
